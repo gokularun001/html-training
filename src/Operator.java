6b2ce42c2fcfd6b689b8c383916defbf9d5e746b
gokularun001 <gokularun20030603@gmail.com>
2024-12-26 20:15:04 -0800

    Learning operators

---
+++ src/Operator.java
@@ -0,0 +1,27 @@
+public class Operator
+{
+    public static void main(String[] args) {
+        int a=10,b=5;
+        //1)Arithmetic operators
+        //int c=a+b; sum of a and b is 15
+        //int c=a-b; subtract of a and b is 5
+        //int c=a/b; division of a and b is 2
+        //int c=a*b; multiple of a and b is 50
+        //int c=a%b; modules of a and b is 0
+        //2)Relational operators
+        // boolean b1 a>b is true;a<b is false;a==b is false;a!=b is true;
+        //a<=b is false;a>=b is true;
+        //3)Logical operators
+        //boolean x=true,y=false;
+        //And(&&) is one false also false;Or(||) is one is true also true;Not(!) x is true means false, y is true means true;
+        //boolean b1=a>b&&a>b; true
+        //4)Assignment operators
+        int i=4;
+        //i+=5; 9
+        //i-=5; -1
+        //i*=5; 20
+        //i/=5; 0
+        i%=5; 4
+        System.out.println(i);
+    }
+}
